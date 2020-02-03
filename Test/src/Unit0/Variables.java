6203ada043593f6b95f973d947242f26ff88e916
gankcaptain <gankcaptain@172.19.219.102>
2020-02-02 20:27:50 -0800

    completed variables assignment

--- Test/src/Unit0/Variables.java
+++ Test/src/Unit0/Variables.java
@@ -19,26 +19,48 @@ public class Variables
 
 		//integer variables
 		byte byteOne = 127;
-
-
+		short shortOne = -32000;
+		int intOne = 123213123;
+		long longOne = 888888888;
+		
 		//decimal variables
-
+		float floatOne = 0.1234567f;
+		double doubleOne = 0.1234567890123456;
 
 		//other integer types
-
-
+		char charOne = 'E';
+		
 		//other types
-
+		boolean booleanOne = false;
+		String stringOne = "Is this just giving examples?";
 
 		//output your information here
 		System.out.println("/////////////////////////////////");
 		System.out.println("*                               *");
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
-		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
-
-
-
+		System.out.println("*byteOne = "+byteOne+"          *");
+		System.out.println("*shortOne = "+shortOne+"        *");
+		System.out.println("*intOne = "+intOne+"            *");
+		System.out.println("*longOne = "+longOne+"          *");
+		System.out.println("*                               *");
+		System.out.println("*                               *");
+		System.out.println("*         real types           *");
+		System.out.println("*                               *");
+		System.out.println("*floatOne = "+floatOne+" *");
+		System.out.println("*doubleOne = "+doubleOne+"    *");
+		System.out.println("*                               *");
+		System.out.println("*                               *");
+		System.out.println("*        other integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*charOne = "+charOne+" *");
+		System.out.println("*                               *");
+		System.out.println("*                               *");
+		System.out.println("*        other types          *");
+		System.out.println("*                               *");
+		System.out.println("*booleanOne = "+booleanOne+" *");
+		System.out.println("*stringOne = "+stringOne+" *");
+		System.out.println("/////////////////////////////////");
 
 
 
