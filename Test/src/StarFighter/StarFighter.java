3d4408b45b79d608a59416bdd3fca1c03346bc5d
gankcaptain <gankcaptain@10.0.0.6>
2020-05-24 20:23:46 -0700

    Finished StarFighter!

---
+++ Test/src/StarFighter/StarFighter.java
@@ -0,0 +1,30 @@
+package StarFighter;
+
+import javax.swing.JFrame;
+import java.awt.Component;
+
+public class StarFighter extends JFrame
+{
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+
+	public StarFighter()
+	{
+		super("StarFighter Game");
+		setSize(WIDTH,HEIGHT);
+
+		OuterSpace theGame = new OuterSpace();
+		((Component)theGame).setFocusable(true);
+
+		getContentPane().add(theGame);
+
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	public static void main( String args[] )
+	{
+		System.out.println("StarFighter game is running");
+		StarFighter run = new StarFighter();
+	}
+}
